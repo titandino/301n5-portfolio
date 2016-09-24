e111c5b97f199eccce25fe9617e3873bbfb958fc
Trenton Kress <trenton.kress@gmail.com>
2016-09-24 03:50:23 -0700

    jQuery CDN added and object constructor for projects started.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function Project(name, date, desc, img) {
+  this.name = name;
+  this.date = date;
+  this.desc = desc;
+  this.img = img;
+}
+
+Project.prototype.render = function() {
+
+};
