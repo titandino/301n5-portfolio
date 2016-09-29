d2f6093afbaf5f78a267dbef1b31aa61a196380c
Trenton Kress <trenton.kress@gmail.com>
2016-09-28 18:31:02 -0700

    Basic layout css work

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 var renderer = { }
 
 renderer.initNav = function() {
-  $('.section-toggle').on('click', 'li', function(e) {
+  $('.section-toggle').on('click', function(e) {
     e.preventDefault();
     $('.tab-section').hide();
-    $('#' + $(this).name).show();
+    $('#' + $(this).data('tab')).fadeIn(500);
   });
+  $('.section-toggle:first').trigger('click');
 };
 
 renderer.initNav();
