e27902bb5df5e089ce0a6f8465a80cdc7c17759b
Trenton Kress <trenton.kress@gmail.com>
2016-10-04 19:26:11 -0700

    Encapsulated main script functions with an iife. Looks really unorganized to me but meh

--- js/script.js
+++ js/script.js
@@ -1,70 +1,70 @@
 'use strict';
 
-var renderer = {
-  projects: [],
-  projectsLoaded: 0,
-};
+(function() {
+  var numLoaded = 0;
+  var projects = [];
 
-renderer.initNav = function() {
-  $('.section-toggle').on('click', function(e) {
-    e.preventDefault();
-    $('.tab-section').hide();
-    $('#' + $(this).data('tab')).fadeIn(500);
-  });
-  $('.section-toggle:first').trigger('click');
-};
-
-renderer.initSkillsLists = function() {
-  $('.skills-list').on('click', 'ul', function(e) {
-    var target = $(this);
-    e.preventDefault();
-    $('.skills-list ul > li').each(function() {
-      if ($(this).is(':visible') && $(this).parent().text() != target.text()) {
-        $(this).toggle(400);
-      }
+  function initNav() {
+    $('.section-toggle').on('click', function(e) {
+      e.preventDefault();
+      $('.tab-section').hide();
+      $('#' + $(this).data('tab')).fadeIn(500);
     });
-    target.find('li').toggle(400);
-  });
-};
+    $('.section-toggle:first').trigger('click');
+  };
 
-renderer.initProjectFlips = function() {
-  $('#projects').on('click', '.proj-body-container', function() {
-    var target = $(this);
-    $('.proj-body-container').each(function() {
-      if ($(this).find('.proj-back').is(':visible') && $(this).text() != target.text()) {
-        $(this).find('.proj-front').toggle(400);
-        $(this).find('.proj-back').toggle(400);
-      }
+  function initSkillsLists() {
+    $('.skills-list').on('click', 'ul', function(e) {
+      var target = $(this);
+      e.preventDefault();
+      $('.skills-list ul > li').each(function() {
+        if ($(this).is(':visible') && $(this).parent().text() != target.text()) {
+          $(this).toggle(400);
+        }
+      });
+      target.find('li').toggle(400);
     });
-    target.find('.proj-front').toggle(400);
-    target.find('.proj-back').toggle(400);
-  });
-};
+  };
 
-renderer.initShowMore = function() {
-  $('#show-more').on('click', function() {
-    renderer.loadProjects(4);
-  });
-};
+  function initProjectFlips() {
+    $('#projects').on('click', '.proj-body-container', function() {
+      var target = $(this);
+      $('.proj-body-container').each(function() {
+        if ($(this).find('.proj-back').is(':visible') && $(this).text() != target.text()) {
+          $(this).find('.proj-front').toggle(400);
+          $(this).find('.proj-back').toggle(400);
+        }
+      });
+      target.find('.proj-front').toggle(400);
+      target.find('.proj-back').toggle(400);
+    });
+  };
 
-renderer.loadProjects = function(amount) {
-  $.getJSON('ajax/projects.json', function(data) {
-    for(var i = 0;i < amount;i++) {
-      if ((renderer.projectsLoaded + i) >= data.length)
-        continue;
-      renderer.projects[renderer.projectsLoaded + i] = new Project(data[renderer.projectsLoaded + i]).render();
-      console.log('Project loaded:', renderer.projectsLoaded + i);
-    }
-    renderer.projectsLoaded += amount;
-  }).fail(function() {
-    console.log('Error loading projects file.');
-  });
-};
+  function initShowMore() {
+    $('#show-more').on('click', function() {
+      loadProjects(4);
+    });
+  };
 
-$(function() {
-  renderer.initNav();
-  renderer.initSkillsLists();
-  renderer.initProjectFlips();
-  renderer.initShowMore();
-  renderer.loadProjects(4);
-});
+  function loadProjects(amount) {
+    $.getJSON('ajax/projects.json', function(data) {
+      for(var i = 0;i < amount;i++) {
+        if ((numLoaded + i) >= data.length)
+          continue;
+        projects[numLoaded + i] = new Project(data[numLoaded + i]).render();
+        console.log('Project loaded:', numLoaded + i);
+      }
+      numLoaded += amount;
+    }).fail(function() {
+      console.log('Error loading projects file.');
+    });
+  };
+
+  $(function() {
+    initNav();
+    initSkillsLists();
+    initProjectFlips();
+    initShowMore();
+    loadProjects(4);
+  });
+})();
