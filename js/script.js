218df24de1b5c091dc780b1ee78156a422629f45
Trenton Kress <trenton.kress@gmail.com>
2016-10-04 19:46:51 -0700

    All code reduced to one file and put inside an iife

--- js/script.js
+++ js/script.js
@@ -4,6 +4,23 @@
   var numLoaded = 0;
   var projects = [];
 
+  function Project(data) {
+    if (typeof data === 'object') {
+      console.log('Parsing object fields.');
+      for (var field in data) {
+        this[field] = data[field];
+      }
+    } else {
+      console.log('Error loading Project.', data);
+    }
+  }
+
+  Project.prototype.render = function() {
+    var template = Handlebars.compile($('#project-template').html());
+    $('.project-display').append(template(this));
+    return this;
+  };
+
   function initNav() {
     $('.section-toggle').on('click', function(e) {
       e.preventDefault();
