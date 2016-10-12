eb78f4ef6d966154962a704cd4e3a5f8f2cc1c20
Trenton Kress <trenton.kress@gmail.com>
2016-10-12 16:27:54 -0700

    Add project fully functional

--- js/admin.js
+++ js/admin.js
@@ -2,12 +2,15 @@
 
 (function(ctx) {
 
+  let template = Handlebars.compile($('#project-template').html());
+
   function loggedIn() {
     return localStorage.access_token && parseInt(localStorage.token_expiry) > Date.now();
   }
 
   function initLoginForm() {
     $('.form-login').on('submit', function(e) {
+      $('.form-login').attr('disabled', true);
       e.preventDefault();
       let data = $(this).serialize();
       $.post('http://localhost/api/login', data, function(res) {
@@ -17,15 +20,58 @@
             localStorage.token_expiry = res.expiresIn;
             initNavTabs();
           } else {
-            $('.login-result').text(res.message);
+            displayLoginResult(res.message);
           }
         } else {
-          $('.login-result').text('Failed to retrieve token from API.');
+          displayLoginResult('Failed to retrieve token from API.');
         }
       }, 'json').fail(function() {
-        $('.login-result').text('Failed to connect to API.');
+        displayLoginResult('Failed to connect to API.');
       });
     });
+  }
+
+  function displayLoginResult(message) {
+    $('.form-login').attr('disabled', false);
+    $('.login-result').text(message);
+  }
+
+  function initAddForm() {
+    $('.form-add-project').on('submit', function(e) {
+      e.preventDefault();
+      $('.form-add-project').attr('disabled', true);
+
+      let data = $(this).serialize();
+
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost/api/projects',
+        data: 'token=' + localStorage.access_token + '&' + data,
+        success: function(msg) {
+          if (msg.includes('Successfully'))
+            $('.form-add-project').trigger('reset');
+          $('.form-add-project').attr('disabled', true);
+          $('.add-result').text(msg);
+        }
+      });
+    });
+    $('.form-add-project').on('change', function() {
+      $('.project-preview').empty();
+      $('.project-preview').append(template(formToJSON($(this))));
+    });
+    initProjectFlips();
+  }
+
+  function formToJSON(form) {
+    let data = {};
+    form.serializeArray().map(function(x) {
+      data[x.name] = x.value;
+    });
+    return data;
+  }
+
+  function initEditForm() {
+
   }
 
   function initNavTabs() {
@@ -35,6 +81,20 @@
       $('#' + $(this).data('tab')).fadeIn(500);
     });
     $('.section-toggle:first').trigger('click');
+  }
+
+  function initProjectFlips() {
+    $('.project-preview').on('click', '.proj-body-container', function() {
+      var target = $(this);
+      $('.proj-body-container').each(function() {
+        if ($(this).find('.proj-back').is(':visible') && $(this).text() != target.text()) {
+          $(this).find('.proj-front').toggle(400);
+          $(this).find('.proj-back').toggle(400);
+        }
+      });
+      target.find('.proj-front').toggle(400);
+      target.find('.proj-back').toggle(400);
+    });
   }
 
   function displayLogin() {
